46a12d01b747b32cf1dc1c3395b3c9d90da07158
Jaques <jaquesrodrigues545@gmail.com>
2024-11-22 00:47:46 -0300

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
+import Home from "./pages/Home/Home";
 import "./App.css";
 import PlayerInfo from "./component/PlayerInfo";
 
 function App() {
   return (
     <>
-      <PlayerInfo playerId={"GQ299UJ99"} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/player/:playerId" element={<PlayerInfo />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
-
-//JQVQYVY
-//GQ299UJ99
